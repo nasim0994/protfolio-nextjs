074ca27624e8ad4953550315dbc8e7e97e6fa430
nasim0994 <nasimuddin0994@gmail.com>
2023-09-26 10:21:01 +0600

    setup project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#ec4d26",
+        secondary: "#FBDBD4",
+        accent: "#4f4f50",
+        neutral: "#111",
+        "neutral-content": "#6e6e6e",
+        "base-100": "#ffffff",
+      },
+      fontFamily: {
+        jerseyM54: ["jerseyM54"],
+        dinMedium: ["dinMedium"],
       },
     },
   },
   plugins: [],
-}
+};
